aa1cf9afa7955b18ff8578d91eba1437855f014b
AnnyDIAN <1979312JAY@gmail.com>
2015-07-20 15:14:40 +0800

    commit all

---
+++ sketch_jul20a/sketch_jul20a.ino
@@ -0,0 +1,63 @@
+void setup() {
+  // put your setup code here, to run once:
+ 
+
+
+      
+     
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+  
+ 
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+ 
+  
+ 
+ 
+  
+  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(50);              // wait for a second
+ digitalWrite(3, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(50);              // wait for a second
+digitalWrite(4, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(50);              // wait for a second
+digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(50);              // wait for a second
+  digitalWrite(6, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(50);              // wait for a second
+digitalWrite(7, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(50);              // wait for a second
+digitalWrite(8, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(50);              // wait for a second
+digitalWrite(9, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(50);              // wait for a second
+  digitalWrite(9,LOW);
+  delay(50);
+   digitalWrite(8,LOW);
+   delay(50);
+   digitalWrite(7,LOW);
+  delay(50);
+  digitalWrite(6,LOW);
+  delay(50); 
+  digitalWrite(5,LOW);
+  delay(50); 
+  digitalWrite(4,LOW);
+  delay(50);
+   digitalWrite(3,LOW);
+  delay(50); 
+  digitalWrite(2,LOW);
+  delay(50);
+ 
+ 
+
+} 
